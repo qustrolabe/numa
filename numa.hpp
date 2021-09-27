e84e5861ad402408f211b9fac328ff844468706e
Nathman999 <nathman9999@gmail.com>
2021-09-27 17:24:01 +0300

    Added dot product function

--- numa.hpp
+++ numa.hpp
@@ -7,19 +7,23 @@
 #include <iostream>
 #include <iomanip>
 
+#include <stdexcept>
+
 #include <vector>
 #include <iterator>
 
 #include <algorithm>
 #include <random>
 
+double real_rand();
+int int_rand(); // UNIMPLEMENTED
 
 
 double real_rand() {
     static std::random_device rd;
     static std::mt19937 gen(rd());
-    static std::uniform_real_distribution<> dis(-1.0, 1.0);
-    // static std::uniform_int_distribution<> dis(0, 3);
+    static std::uniform_real_distribution<> dis(0, 1.0);
+    // static std::uniform_int_distribution<> dis(0, 9);
     return dis(gen);
 }
 
@@ -44,10 +48,17 @@ public:
 
 public:
     auto &get_data() { return data; }
+
     const auto &get_data() const { return data; }
     const auto &get(int y, int x) const { return data.at(x + y * cols); }
+
+    const auto get_rows() const { return rows; }
+    const auto get_cols() const { return cols; }
+
     auto &set(int y, int x, const T &val) { return data.at(x + y * cols) = val; }
     auto &set(int y, int x) { return data.at(x + y * cols); }
+
+
 
     friend std::ostream &operator<<(std::ostream &str, const matrix &m) {
         for(size_t i = 0; i < m.rows; i++) {
@@ -67,28 +78,9 @@ public:
 
     // DOT PRODUCT OF TWO MATRICES
     // TODO: MOVE TO EXTERNAL FUNCTION
+
     friend matrix operator*(const matrix &A, const matrix &B) {
-        // A.cols == B.rows
-        // C {A.rows, B.cols}
-
-        if(A.cols != B.rows) {
-            //Throw error
-            std::cout << "Wrong demensions!!\n";
-        }
-
-        auto C = matrix(A.rows, B.cols);
-
-        for (int i = 0; i < C.rows; ++i) {
-            for (int j = 0; j < C.cols; ++j) {
-                for (int l = 0; l < A.cols; ++l) {
-                    C.set(i, j) += (A.get(i, l) * B.get(l, j));
-                }
-            }
-        }
-
-        std::cout << C << "\n";
-
-        return matrix(1, 1);
+        return dot_product(A, B);
     }
 
 };
@@ -99,4 +91,23 @@ matrix<double> randMatrix(int h, int w) {
     std::generate(data.begin(), data.end(), real_rand);
     return m;
     // return std::move(m); // ?
+}
+
+template<class T = double>
+matrix<T> dot_product(const matrix<T> &A, const matrix<T> &B) {
+    if(A.get_cols() != B.get_rows()) {
+        throw std::invalid_argument("Wrong matrix dimensions.");
+    }
+
+    auto C = matrix(A.get_rows(), B.get_cols());
+
+    for (int i = 0; i < C.get_rows(); ++i) {
+        for (int j = 0; j < C.get_cols(); ++j) {
+            for (int l = 0; l < A.get_cols(); ++l) {
+                C.set(i, j) += (A.get(i, l) * B.get(l, j));
+            }
+        }
+    }
+
+    return C;
 }
